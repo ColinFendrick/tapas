63452fac47ae3c88fcadb38ac3594dec828dd952
Colin Fendrick <colin.fendrick@gmail.com>
2017-05-08 17:18:08 -0400

    Fixed store issues

--- src/store.js
+++ src/store.js
@@ -1,23 +1,11 @@
 import { observable } from 'mobx'
 
 class Store {
-  @observable dropDown = {
-    'Menu': 'none',
-    'About Us': 'none',
-    'Locations': 'inline',
-    'Contact Us': 'none'
-  }
+  @observable dropDown = {}
 
   hoverItem = name => {
-    // console.log(name)
-    this.dropDown = {
-      'Menu': 'none',
-      'About Us': 'none',
-      'Locations': 'inline',
-      'Contact Us': 'none'
-    }
-    // this.dropDown = {...this.dropDown, `${name}`: 'inline'}
-    // console.log(this.dropDown)
+    this.dropDown = {}
+    this.dropDown = {[name]: 'inline'}
   }
 }
 
