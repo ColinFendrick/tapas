f2ebc0e8340b386fbed84f686dc4fc4107f1f3db
Colin Fendrick <colin.fendrick@gmail.com>
2017-05-08 23:35:49 -0400

    Lets do something

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => (
+  <div className='Body'>
+    thE BODY
+  </div>
+)
+
+export default Body
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,9 @@ const Header = () => (
   <div className='Header'>
     <div className='title'>
       <h1>Tapas the Mornin to Jah!</h1>
+      <h3>A Jamaican Irish Spanish Small-Plate Breakfast Restaurant, For You!!</h3>
+      <img className='brazil' src='http://bestanimations.com/Flags/SouthAmerica/Brazil/brazilian-flag-animated-gif-3.gif' alt='Brazilian Flag' />
+      <img className='ireland' src='http://www.abflags.com/_flags/flags-of-the-world/Ireland%20flag/Ireland%20flag-XXL-anim.gif' alt='Irish Flag' />
     </div>
     <NavBar />
   </div>
